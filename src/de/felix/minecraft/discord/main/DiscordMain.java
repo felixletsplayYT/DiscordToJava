2af36a3a2b9a931836a5dd5e3ea80619f3051354
felixletsplay <felixletsplay@outlook.de>
2017-02-10 18:57:39 +0100

    Initial commit

---
+++ src/de/felix/minecraft/discord/main/DiscordMain.java
@@ -0,0 +1,13 @@
+package de.felix.minecraft.discord.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main Plugin
+ * Created by felix on 10.02.2017.
+ */
+public class DiscordMain extends JavaPlugin {
+    public void onEnable(){
+
+    }
+}
